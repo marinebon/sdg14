db9fc5545d5436f6f95a49bbb057d025407bac05
Ben Best <ben@ecoquants.com>
2017-10-01 01:59:29 +0000

    got CMARS obis, wdpa next

--- technical/obis-eez.metric.R
+++ technical/obis-eez.metric.R
@@ -26,8 +26,14 @@ dir_root = switch(
 dir_data = file.path(dir_root, 'biodiversity')
 eez_all_rdata    = file.path(dir_data, 'eez_all.rdata')
 eez_smp_rdata    = file.path(dir_data, 'eez_smp.rdata')
+wdpa_dir = file.path(dir_root, 'wdpa')
+wdpa_zip = file.path(wdpa_dir, 'WDPA_Sep2017-shapefile.zip')
+wdpa_shp = file.path(wdpa_dir, 'WDPA_Sep2017-shapefile-polygons.shp')
+wdpa_url = 'https://www.protectedplanet.net/downloads/WDPA_Sep2017?type=shapefile'
 
 obis_cols_drop = c(
+  # ID fields:
+  # 'id','institutionID','collectionID','datasetID','eventID','identificationID','individualID','locationID','materialSampleID','occurrenceID','scientificNameID','taxonConceptID'
   'acceptedNameUsage','acceptedNameUsageID','accessRights','associatedMedia','associatedReferences','associatedSequences','associatedTaxa',
   'basisOfRecord','behavior','bibliographicCitation',
   'collectionID','continent','coordinatePrecision','coordinateUncertaintyInMeters','countryCode','county',
@@ -41,7 +47,7 @@ obis_cols_drop = c(
   'language','lifeStage','locality','locationAccordingTo','locationID','locationRemarks',
   'materialSampleID','maximumDepthInMeters','minimumDepthInMeters','modified','municipality',
   'nameAccordingTo','nameAccordingToID','namePublishedIn','namePublishedInID',
-  'occurrenceID','occurrenceRemarks','occurrenceStatus','originalNameUsage','originalNameUsageID','otherCatalogNumbers','ownerInstitutionCode',
+  'occurrenceRemarks','occurrenceStatus','originalNameUsage','originalNameUsageID','otherCatalogNumbers','ownerInstitutionCode',
   'parvorder',
   'section',
   'recordNumber','recordedBy','references','reproductiveCondition','rights','rightsHolder',
@@ -95,18 +101,28 @@ eez_smp = readRDS(eez_smp_rdata)
 #   addTiles() %>%
 #   addPolygons()
 
+# fetch-wdpa
+if (!file.exists(wdpa_shp)){
+  download.file(wdpa_url, wdpa_zip)
+  unzip(wdpa_zip, exdir=wdpa_dir)
+}
+wdpa_all = read_sf(wdpa_shp) # WHOAH! 3.5 GB shp
+
 # fetch-obis-by-eez ----
 territories = eez_smp %>% filter(pol_type == '200NM') %>% .$territory1
 #for (ter in territories){
 for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
-
+  
   #ter = 'Galapagos' # ter = 'Colombia' # sort(eez_smp$territory1)
   cat(sprintf('%s - %s\n', ter, Sys.time()))
   ter_f = str_replace_all(ter, ' ', '_')
-
+  
   wkt_txt  = sprintf('%s/eez-%s_wkt.txt', dir_data, ter_f)
   obis_geo = sprintf('%s/eez-%s_obis.geojson', dir_data, ter_f)
   obis_csv = sprintf('%s/eez-%s_obis.csv', dir_data, ter_f)
+  wdpa_geo = sprintf('%s/eez-%s_wdpa.geojson', dir_data, ter_f)
+  obis_wdpa_csv        = sprintf('%s/eez-%s_obis_wdpa.csv', dir_data, ter_f)
+  obis_wdpa_metric_txt = sprintf('%s/eez-%s_obis_wdpa_metric.txt', dir_data, ter_f)
   
   if (!file.exists(wkt_txt)){
     wkt = eez_smp %>%
@@ -140,15 +156,17 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
     # fetch OBIS occurrences
     obis_tbl = robis2::occurrence(geometry=wkt) %>%
       as_tibble() %>%
-      rename(
-        occurrence_id = id) %>%
       select(
-        kingdom, taxonomicgroup, scientificName, year, occurrence_id,
+        id, kingdom, taxonomicgroup, scientificName, year,
         eventDate, decimalLongitude, decimalLatitude, depth,
         taxonRank, worms_id, valid_id, originalscientificname,
         resource_id, catalogNumber, institutionCode, collectionCode,
         qc) %>%
-      arrange(kingdom, taxonomicgroup, scientificName, year, occurrence_id) # View(obis_tbl)
+      arrange(kingdom, taxonomicgroup, scientificName, year, id) %>%
+      mutate(
+        occurrence_id = sprintf('obis:%s:%d', ter, row_number())) # TODO: unique ID in OBIS?!
+    
+    # View(obis_tbl)
     
     # drop unneeded OBIS columns: moot with select() above, retained for explicitness of dropped columns to revisit
     obis_tbl = obis_tbl %>%
@@ -164,22 +182,22 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
       mutate(
         invalid = valid_id != id) %>%
       arrange(scientificName,invalid,worms_id,redlist,status,phylum,class,order,family,genus,species) %>% # View(cklist)
-      group_by(scientificName) %>%
+      group_by(worms_id) %>%
       summarize(
-        checklist_id = first(id),
-        worms_id     = first(worms_id),
-        valid_id     = first(valid_id),
-        parent_id    = first(parent_id),
-        rank_name    = first(rank_name),
-        tauthor      = first(tauthor),
-        phylum       = first(phylum),
-        class        = first(class),
-        order        = first(order),
-        family       = first(family),
-        genus        = first(genus),
-        species      = first(species),
-        redlist      = first(redlist),
-        status       = first(status)) %>%
+        checklist_id   = first(id),
+        scientificName = first(scientificName),
+        valid_id       = first(valid_id),
+        parent_id      = first(parent_id),
+        rank_name      = first(rank_name),
+        tauthor        = first(tauthor),
+        phylum         = first(phylum),
+        class          = first(class),
+        order          = first(order),
+        family         = first(family),
+        genus          = first(genus),
+        species        = first(species),
+        redlist        = first(redlist),
+        status         = first(status)) %>%
       mutate(
         invalid = valid_id != checklist_id) %>%
       arrange(invalid,scientificName,worms_id,redlist,status,phylum,class,order,family,genus,species)
@@ -207,7 +225,8 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
           obis_sf)[[1]])
     
     # write obis_geo with just id to save disk space, for later rejoining with obis_tbl
-    if (sum(duplicated(obis_tbl$occurrence_id)) > 0) stop('Whoah! Expecting all unique ids in OBIS occurrence table.')
+    if (sum(duplicated(obis_tbl$occurrence_id)) > 0) 
+      stop('Whoah! Expecting all unique ids in OBIS occurrence table.')
     obis_sf %>%
       select(occurrence_id) %>%
       write_sf(obis_geo, delete_dsn=T)
@@ -236,37 +255,25 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
   #   st_set_geometry(NULL) %>%
   #   head()
   
-  # fetch-wdpa ----
-  wdpa_geo = sprintf('%s/eez-%s_wdpa.geojson', dir_data, ter_f)
+  # TODO: wdpa later
+  #next()
   
+  # extract-wdpa-by-eez ----
   eez_sf = eez_all %>%
     filter(
       pol_type == '200NM',
       territory1==ter)
   
   if (!file.exists(wdpa_geo)){
-    bb = st_bbox(eez_sf)
     
-    q_url = 'http://ec2-54-204-216-109.compute-1.amazonaws.com:6080/arcgis/rest/services/wdpa/wdpa/MapServer/1/query'
-    res = httr::GET(q_url, query = list(
-      f            = 'json',
-      geometry     = sprintf('%0.1f,%0.1f,%0.1f,%0.1f', bb[['xmin']], bb[['ymin']], bb[['xmax']], bb[['ymax']]),
-      geometryType = 'esriGeometryEnvelope',
-      inSR         = 4326,
-      outSR        = 4326,
-      outFields    = '*'))
-    
-    wdpa_sf = httr::content(res) %>%
-      read_sf() # wdpa0_sf = wdpa_sf; mapview(wdpa0_sf)
+    # filter by eez's within eez
+    wdpa_sf = wdpa_all %>%
+      slice(st_intersects(eez_sf, wdpa_all)[[1]])
     
     if (nrow(wdpa_sf) == 0){
       warning('no wdpa')
       write_sf(wdpa_sf, wdpa_geo, delete_dsn=T)
     } else {
-      
-      # filter by eez's within eez
-      wdpa_sf = wdpa_sf %>%
-        slice(st_intersects(eez_sf, wdpa_sf)[[1]])
       
       # calculate original area before extracting to eez
       wdpa_sf = wdpa_sf %>%
@@ -309,10 +316,8 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
           weight = 5,
           bringToFront = T))
   }
-
-  # join-obis-wdpa ----
-  obis_wdpa_csv = sprintf('%s/eez-%s_obis_wdpa.csv', dir_data, ter_f)
   
+  # join-obis-wdpa ----
   if (!file.exists(obis_wdpa_csv)){
     
     # join fields with prefix identifying source
@@ -330,71 +335,71 @@ for (ter in c('Galapagos','Colombia','Costa Rica','Ecuador','Panama')){
   }
   obis_wdpa = read_csv(obis_wdpa_csv)
   
-  # obis_wdpa %>%
-  #   head()
-  
   # calc-metric ----
-  
-  # summarize by single OBIS occurrence_id
-  obis1_wdpa = obis_wdpa %>%
-    group_by(occurrence_id) %>%
-    summarize(
-      wdpa_pids = paste(WDPA_PID, collapse=',')) # populate list of WDPA park ids
-  
-  # see Selig et al. - 2013 - Assessing Global Marine Biodiversity Status within a Coupled Socio-Ecological Perspective
-  wts_status = c(LC=0, NT=0.2, VU=0.4, EN=0.6, CR=0.8, EX=1)
-  
-  d = obis_sf %>%
-    st_set_geometry(NULL) %>% # drop spatial
-    left_join(
-      obis1_wdpa,
-      by='occurrence_id') %>%
-    filter(
-      !is.na(status),
-      status %in% names(wts_status)) %>%
-    mutate(
-      wdpa_pids = ifelse(wdpa_pids=='NA', NA, wdpa_pids),  # weird 'NA', not NA
-      in_mpa    = ifelse(!is.na(wdpa_pids), TRUE, FALSE),
-      status_wt = wts_status[status]) %>%
-    group_by(
-      taxonomicgroup, in_mpa) %>%
-    summarise(
-      status_wt         = mean(status_wt), # NOTE: weighted by number of occurrences
-      n_scientificNames = length(unique(scientificName)),
-      n_occurrences     = n()) # %>% ungroup()
-  # d
-  # sum(d$n_occurrences)
-  # nrow(obis_sf)
-  
-  d_dif = d %>%
-    select(taxonomicgroup, in_mpa, status_wt) %>%
-    spread(in_mpa, status_wt) %>%
-    rename(
-      avg_status_wt_outside_mpa = `FALSE`,
-      avg_status_wt_inside_mpa = `TRUE`) %>%
-    mutate(
-      status_dif = avg_status_wt_outside_mpa - avg_status_wt_inside_mpa)
-  # fully unprotected (out - in): 1   -   0 =  1
-  # evenly  protected (out - in): 0.5 - 0.5 =  0
-  # fully   protected (out - in): 0   -   1 = -1
-  # d_dif
-  
-  d_dif = d_dif %>%
-    filter(
-      !is.na(status_dif))
-  
-  wts_taxa = d %>%
-    group_by(taxonomicgroup) %>%
-    summarize(
-      n_scientificNames = sum(n_scientificNames)) %>% # NOTE: repeats in/out mpa
-    mutate(
-      wt = rescale(n_scientificNames))
-  # wts_taxa
-  
-  weighted.mean(
-    x = d_dif$status_dif, 
-    w = setNames(
-      wts_taxa$wt,
-      wts_taxa$taxonomicgroup)[
-        d_dif$taxonomicgroup])
+  if (!file.exists(obis_wdpa_metric_txt)){
+    # summarize by single OBIS occurrence_id
+    obis1_wdpa = obis_wdpa %>%
+      group_by(occurrence_id) %>%
+      summarize(
+        wdpa_pids = paste(WDPA_PID, collapse=',')) # populate list of WDPA park ids
+    
+    # see Selig et al. - 2013 - Assessing Global Marine Biodiversity Status within a Coupled Socio-Ecological Perspective
+    wts_status = c(LC=0, NT=0.2, VU=0.4, EN=0.6, CR=0.8, EX=1)
+    
+    d = obis_sf %>%
+      st_set_geometry(NULL) %>% # drop spatial
+      left_join(
+        obis1_wdpa,
+        by='occurrence_id') %>%
+      filter(
+        !is.na(status),
+        status %in% names(wts_status)) %>%
+      mutate(
+        wdpa_pids = ifelse(wdpa_pids=='NA', NA, wdpa_pids),  # weird 'NA', not NA
+        in_mpa    = ifelse(!is.na(wdpa_pids), TRUE, FALSE),
+        status_wt = wts_status[status]) %>%
+      group_by(
+        taxonomicgroup, in_mpa) %>%
+      summarise(
+        status_wt         = mean(status_wt), # NOTE: weighted by number of occurrences
+        n_scientificNames = length(unique(scientificName)),
+        n_occurrences     = n()) # %>% ungroup()
+    # d
+    # sum(d$n_occurrences)
+    # nrow(obis_sf)
+    
+    d_dif = d %>%
+      select(taxonomicgroup, in_mpa, status_wt) %>%
+      spread(in_mpa, status_wt) %>%
+      rename(
+        avg_status_wt_outside_mpa = `FALSE`,
+        avg_status_wt_inside_mpa = `TRUE`) %>%
+      mutate(
+        status_dif = avg_status_wt_outside_mpa - avg_status_wt_inside_mpa)
+    # fully unprotected (out - in): 1   -   0 =  1
+    # evenly  protected (out - in): 0.5 - 0.5 =  0
+    # fully   protected (out - in): 0   -   1 = -1
+    # d_dif
+    
+    d_dif = d_dif %>%
+      filter(
+        !is.na(status_dif))
+    
+    wts_taxa = d %>%
+      group_by(taxonomicgroup) %>%
+      summarize(
+        n_scientificNames = sum(n_scientificNames)) %>% # NOTE: repeats in/out mpa
+      mutate(
+        wt = rescale(n_scientificNames))
+    # wts_taxa
+    
+    m = weighted.mean(
+      x = d_dif$status_dif, 
+      w = setNames(
+        wts_taxa$wt,
+        wts_taxa$taxonomicgroup)[
+          d_dif$taxonomicgroup])
+    
+    write_lines(m, obis_wdpa_metric_txt)
+  }
 }
